df91a1a6d7e3e370441201072ab1c9b0c84a2ca0
Tatyana Amosova <atgatg@bk.ru>
2023-06-07 23:14:28 +0300

    classes changed

--- src/Character.js
+++ src/Character.js
@@ -1,36 +1,19 @@
 export default class Character {
   constructor(name, type) {
-    const nameLength = name.length;
-    if (typeof name === 'string' && nameLength >= 2 && nameLength <= 10) {
-      this.name = name;
-    } else {
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
+    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
       throw new Error('Длина имени должна быть от 2-х до 10 символов');
     }
 
-    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
     if (!types.includes(type)) {
-      throw new Error('Разрешенные персонажи: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
+      const typesString = types.join(', ');
+      throw new Error(`Разрешенные персонажи: ${typesString}`);
     }
-    this.type = type;
 
+    this.name = name;
+    this.type = type;
     this.health = 100;
     this.level = 1;
-
-    switch (type) {
-      case 'Bowman':
-      case 'Undead':
-        this.attack = 25;
-        this.defence = 25;
-        break;
-      case 'Swordsman':
-      case 'Zombie':
-        this.attack = 40;
-        this.defence = 10;
-        break;
-      default:
-        this.attack = 10;
-        this.defence = 40;
-        break;
-    }
   }
 }
